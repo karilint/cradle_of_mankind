53a5c4874e65ff3e30772199b413b65b7d35f96a
Jonne Sotala <jonne.sotala@gmail.com>
2020-11-03 16:12:45 +0200

    email port fixed

--- cradle_of_mankind/settings.py
+++ cradle_of_mankind/settings.py
@@ -137,7 +137,7 @@ LOGIN_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_POST = 587
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = config.get('EMAIL_PASS')
