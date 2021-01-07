09eba4e10462aa0f491fe843ca1c05106f4bbd4b
Jonne Sotala <jonne.sotala@gmail.com>
2021-01-07 13:09:30 +0200

    unique poistettu annotationfieldista

--- quality_control/models.py
+++ quality_control/models.py
@@ -14,7 +14,7 @@ class FinalAnnotation(models.Model):
 
 
 class AnnotationField(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
 
     class Meta:
         ordering = ["name"]
