1da76a2e5c01a074be6bd939d8bb483efd6e475d
Jonne Sotala <jonne.sotala@gmail.com>
2022-08-01 09:38:29 +0300

    templatetag 'split' added
    - it splits strings based on whitespace

--- app/masterdata/templatetags/my_filters.py
+++ app/masterdata/templatetags/my_filters.py
@@ -8,6 +8,11 @@ def _range(number):
     return range(1, number+1)
 
 
+@register.filter(name='split')
+def _split(string):
+    return string.split()
+
+
 @register.filter(name='to_string')
 def to_string(master_data):
     """Takes a master data object and returns a string where all the 
